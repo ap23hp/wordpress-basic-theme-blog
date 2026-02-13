6cf884b6287fe75850f9fa4ad9fe653302361d9e
Apra Khanna <aprakhanna5@gmail.com>
2026-02-13 11:10:04 +0530

    Refactor index.php to use template parts

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo('name'); ?></title>
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-
-<div class="site-header">
-    <h1><?php bloginfo('name'); ?></h1>
-    <p><?php bloginfo('description'); ?></p>
-</div>
+<?php get_header(); ?>
 
 <div class="content">
     <?php
@@ -39,6 +26,4 @@
     ?>
 </div>
 
-<?php wp_footer(); ?>
-</body>
-</html>
+<?php get_footer(); ?>
