c30720c5c4601d96c0627b3aa47432d2dbc33459
Apra Khanna <aprakhanna5@gmail.com>
2026-02-13 11:10:39 +0530

    Add navigation menu registration

--- functions.php
+++ functions.php
@@ -8,4 +8,13 @@ add_action('wp_enqueue_scripts', 'my_theme_styles');
 // Theme support
 add_theme_support('title-tag');
 add_theme_support('post-thumbnails');
+
+// Register navigation menus
+function my_theme_menus() {
+    register_nav_menus(array(
+        'primary' => 'Primary Menu',
+        'footer'  => 'Footer Menu'
+    ));
+}
+add_action('after_setup_theme', 'my_theme_menus');
 ?>
